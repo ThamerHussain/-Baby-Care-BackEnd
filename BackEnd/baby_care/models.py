f7fc0f36e15c37ce716c9d35ef87b489f8490ae5
MROMER444 <omerraad444@gmail.com>
2022-10-01 17:47:28 +0300

    requirments

--- BackEnd/baby_care/models.py
+++ BackEnd/baby_care/models.py
@@ -248,7 +248,7 @@ class Favourite(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.user.first_name } - {self.product}'
+        return f'{self.user.first_name } - {self.product.name}'
 
 
 
